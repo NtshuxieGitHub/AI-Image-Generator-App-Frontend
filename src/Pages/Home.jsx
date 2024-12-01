ffd0017920205c322fbfd0621add80d763070722
ntshuxekom98@gmail.com <ntshuxekom98@gmail.com>
2024-12-01 11:45:10 +0200

    second commit

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -24,12 +24,15 @@ const Home = () => {
     setLoading(true);
 
     try {
-      const response = await fetch("http://localhost:8080/api/v1/post", {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
+      const response = await fetch(
+        "https://ai-image-generator-ncm-3fde5b1720cb.herokuapp.com/api/v1/post",
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
 
       if (response.ok) {
         const result = await response.json();

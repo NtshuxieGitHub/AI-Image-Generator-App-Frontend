2d745f252b622513a39b1d02963d268acde6232a
Ntshuxeko Confidence Mathebula <114024522+NtshuxieGitHub@users.noreply.github.com>
2025-06-09 20:29:20 +0200

    Update Home.jsx
    update backend url

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -25,7 +25,7 @@ const Home = () => {
 
     try {
       const response = await fetch(
-        "https://ai-image-generator-ncm-3fde5b1720cb.herokuapp.com/api/v1/post",
+        "https://ai-image-generator-app-backend.onrender.com//api/v1/post",
         {
           method: "GET",
           headers: {
